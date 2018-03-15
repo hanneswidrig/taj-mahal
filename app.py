ea21f3b2cbcedc807a0e12be54155466faf5343c
hanneswidrig <hanneswidrig@gmail.com>
2018-03-15 12:03:16 -0400

    Minor adjustment to navigation bar

--- app.py
+++ app.py
@@ -20,4 +20,21 @@ def after_request():
 def index():
   return render_template('index.html')
 
+@app.route('/listing')
+def add_listing():
+	return render_template('all-listings.html')
+
+@app.route('/listing/add')
+def all_listings():
+	return render_template('add-listing.html')
+
+@app.route('/user')
+def all_users():
+	return '<h1>All Users</h1>'
+
+@app.route('/user/<int:user_id>')
+def user_profile(user_id):
+	return 'User ID: {0}'.format(user_id)
+	# return render_template('user-profile.html')
+
 app.run(host='0.0.0.0', port=8080, debug=True)
