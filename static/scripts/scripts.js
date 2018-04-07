ce63f6f1c696cb15154bd6b9e1e528c20730badb
hanneswidrig <hanneswidrig@gmail.com>
2018-04-07 11:28:19 -0400

    Improved contextual hovering on desktop

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-	if (window.location.pathname == "/" || window.location.pathname == "") {
-		$('#index').addClass("nav-item-active");
+	activePageMobile();
+	activePageDesktop();
+	function activePageMobile() {
+		if (window.location.pathname == "/" || window.location.pathname == "") {
+			$('#index').addClass("nav-item-active");
+		}
+		else if (window.location.pathname == "/listing/add") {
+			$('#add').addClass("nav-item-active");
+		}
+		else if (window.location.pathname == "/user") {
+			$('#account').addClass("nav-item-active");
+		}
 	}
-	else if (window.location.pathname == "/listing/add") {
-		$('#add').addClass("nav-item-active");
-	}
-	else if (window.location.pathname == "/user") {
-		$('#account').addClass("nav-item-active");
+	function activePageDesktop() {
+		if (window.location.pathname == "/" || window.location.pathname == "") {
+			$('#index-d').addClass("dsktp-item-active");
+		}
+		else if (window.location.pathname == "/listing/add") {
+			$('#add-d').addClass("dsktp-item-active");
+		}
+		else if (window.location.pathname == "/user") {
+			$('#account-d').addClass("dsktp-item-active");
+		}
 	}
 });
