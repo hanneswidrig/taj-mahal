eca079fc707b55dbbe20b5ac616f239350bc1ba3
hanneswidrig <hanneswidrig@gmail.com>
2018-04-10 22:58:17 -0400

    formatting changes only

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,3 +1,7 @@
+drop table if exists listing;
+drop table if exists category;
+
+
 create table listing
 (
   listing_id         serial                   not null
@@ -21,35 +25,30 @@ create table listing
   date_modified      timestamp with time zone not null
 );
 
-create unique index listing_listing_id_uindex
-  on listing (listing_id);
-
+create unique index listing_listing_id_uindex on listing (listing_id);
 create unique index lower_title_idx on listing ((lower(title)));
 
 
-CREATE TABLE category
+create table category
 (
-  category_id SERIAL      NOT NULL
-    CONSTRAINT category_pkey
-    PRIMARY KEY,
-  name        VARCHAR(64) NOT NULL
+  category_id serial      not null
+    constraint category_pkey
+    primary key,
+  name        varchar(64) not null
 );
 
-CREATE UNIQUE INDEX category_category_id_uindex
-  ON category (category_id);
-
-CREATE UNIQUE INDEX category_name_uindex
-  ON category (name);
+create unique index category_category_id_uindex on category (category_id);
+create unique index category_name_uindex on category (name);
 
 
-CREATE TABLE listing_category
+create table listing_category
 (
-  listing_id  INTEGER NOT NULL
-    CONSTRAINT listing_category_listing_listing_id_fk
-    REFERENCES listing,
-  category_id INTEGER NOT NULL
-    CONSTRAINT listing_category_category_category_id_fk
-    REFERENCES category,
-  CONSTRAINT listing_category_category_id_listing_id_pk
-  PRIMARY KEY (category_id, listing_id)
+  listing_id  integer not null
+    constraint listing_category_listing_listing_id_fk
+    references listing,
+  category_id integer not null
+    constraint listing_category_category_category_id_fk
+    references category,
+  constraint listing_category_category_id_listing_id_pk
+  primary key (category_id, listing_id)
 );
