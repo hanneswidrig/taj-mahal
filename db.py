6171836bba67a1ac9e3ff8b593c775f4b0bc9707
hanneswidrig <hanneswidrig@gmail.com>
2018-05-10 16:50:34 -0400

    better db function

--- db.py
+++ db.py
@@ -182,6 +182,6 @@ def create_user(new_user):
     return g.cursor.rowcount
 
 def get_latest_user_id():
-		query = '''SELECT user_id FROM "user" ORDER BY user_id DESC limit 1;'''
+		query = '''SELECT count(user_id) from "user";'''
 		g.cursor.execute(query)
 		return g.cursor.fetchone()[0] + 1
